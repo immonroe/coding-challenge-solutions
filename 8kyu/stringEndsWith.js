f6b3cad55d02f34c6cc80c47e15d66c58fe169c6
manny <immanuelmonroe@example.com>
2025-03-06 16:07:10 -0500

    Added solution

---
+++ 8kyu/stringEndsWith.js
@@ -0,0 +1,17 @@
+// String ends with?
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+    // TODO: complete
+    if ((str.charAt(str.length - 1)) === ending.charAt(ending.length - 1)) {
+        return true 
+    } else {
+        return false
+    }
+}
