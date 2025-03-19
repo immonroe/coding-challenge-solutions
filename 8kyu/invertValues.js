2f708762406a3d2b2753566373f575cb07352441
immonroe <immanuelmonroe@gmail.com>
2025-03-19 09:50:13 -0400

    Updated week7 folder with invert values exercise solution

---
+++ 8kyu/invertValues.js
@@ -0,0 +1,13 @@
+// Invert Values
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+// [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+// [] --> []
+// You can assume that all values are integers. Do not mutate the input array.
+
+function invert(array) {
+    const map1 = array.map((x) => x * -1);
+    return map1
+}
