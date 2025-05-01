814c436f4f4caab02be079125ff124d72c03dc05
immonroe <immanuelmonroe@gmail.com>
2025-05-01 09:15:26 -0400

    Added solution for products of array except self using postfix array to track before/after array iteration for values to add to product

--- arraysAndHashing/productsOfArrayExceptSelf.js
+++ arraysAndHashing/productsOfArrayExceptSelf.js
@@ -26,19 +26,18 @@ class Solution {
      * @return {number[]}
      */
     productExceptSelf(nums) {
-        // const n = nums.length
-        // const result = new Array(n)
-        const result = []
-        
-        for (let i = 0; i < nums.length; i++) {
-            let prod = 1
-            for (let j = 0; j < nums.length; j++) {
-                if (i !== j) {
-                    prod *= nums[j]
-                }
-            }
-            result[i] = prod
+        const n = nums.length
+        const res = new Array(n).fill(1)
+
+        for (let i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1]
         }
-        return result
+
+        let postfix = 1
+        for (let i = n - 1; i >= 0; i--) {
+            res[i] *= postfix
+            postfix *= nums[i]
+        }
+        return res
     }
 }
