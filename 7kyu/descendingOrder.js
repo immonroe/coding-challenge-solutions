1049e6e7d47540ed9dd53a3f4a13a1e4f112d8d6
immonroe <immanuelmonroe@gmail.com>
2025-05-21 08:31:21 -0400

    Added comment to desc order solution for clarity on sort method

--- 7kyu/descendingOrder.js
+++ 7kyu/descendingOrder.js
@@ -9,7 +9,9 @@
 
 // Input: 123456789 Output: 987654321
 
-function descendingOrder(n){
+function descendingOrder(n) {
     // Turn the input into a string, split the string and remove space, sort the string in ascending order, reverse the order so the highest number are first and lowest are last, join the string, wrap it in Number() to convert string back into a number
     return Number(String(n).split('').sort().reverse().join(''))
+    // the reason why sort does not have to be .sort((a,b) => b - a) is because all of the nums are single digit
+    // great point to bring up in an interview explain reasoning behind what I am doing
 }
