081ddb5daad3abb032ed132f39e228113bb37c55
immonroe <immanuelmonroe@gmail.com>
2025-06-17 08:41:21 -0400

    Updated solution to twoSum using hashmap/one pass to reduce complexity to O(n)

--- arraysAndHashing/twoSum.js
+++ arraysAndHashing/twoSum.js
@@ -34,13 +34,18 @@ class Solution {
      * @param {number} target
      * @return {number[]}
      */
+
+
     twoSum(nums, target) {
+        const prevMap = new Map()
+
         for (let i = 0; i < nums.length; i++) {
-            for (let j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] === target) {
-                    return [i, j]
-                }
+            const diff = target - nums[i]
+            if (prevMap.has(diff)) {
+                return [prevMap.get(diff), i]
             }
+            prevMap.set(nums[i], i)
         }
+        return []
     }
 }
