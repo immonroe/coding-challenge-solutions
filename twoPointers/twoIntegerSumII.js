972e85d2fa2d1c7475eb8a76c3f12f36c9e7cb5d
immonroe <immanuelmonroe@gmail.com>
2025-06-20 09:54:16 -0400

    Updated solution for two int sum ii to two pointer solution

--- twoPointers/twoIntegerSumII.js
+++ twoPointers/twoIntegerSumII.js
@@ -32,13 +32,33 @@ class Solution {
      * @param {number} target
      * @return {number[]}
      */
+
+    // Brute force
+    // twoSum(numbers, target) {
+    //     for (let i = 0; i < numbers.length; i++) {
+    //         for (let j = i + 1; j < numbers.length; j++) {
+    //             if (numbers[i] + numbers[j] === target) {
+    //                 return [i + 1, j + 1]
+    //             }
+    //         }
+    //     }
+    // }
+
+    // two pointer
     twoSum(numbers, target) {
-        for (let i = 0; i < numbers.length; i++) {
-            for (let j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] === target) {
-                    return [i + 1, j + 1]
-                }
+        let l = 0, r = numbers.length - 1
+
+        while (l < r) {
+            const curSum = numbers[l] + numbers[r]
+
+            if (curSum > target) {
+                r--
+            } else if (curSum < target) {
+                l++
+            } else {
+                return [l + 1, r + 1]
             }
         }
+        return []
     }
 }
