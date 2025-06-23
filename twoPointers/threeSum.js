ec374850ca4c9d5dd685fb64a96d00efee58addf
immonroe <immanuelmonroe@gmail.com>
2025-06-23 13:05:05 -0400

    Added optimized solution for 3Sum using two pointers

--- twoPointers/threeSum.js
+++ twoPointers/threeSum.js
@@ -33,23 +33,59 @@
 // -10^5 <= nums[i] <= 10^5
 
 // brute force - may not work depending on test cases
+// class Solution {
+//     /**
+//      * @param {number[]} nums
+//      * @return {number[][]}
+//      */
+//     threeSum(nums) {
+//         const res = new Set()
+//         nums.sort((a,b) => a - b)
+//         for (let i = 0; i < nums.length; i++) {
+//             for (let j = i + 1; j < nums.length; j++) {
+//                 for (let k = j + 1; k < nums.length; k++) {
+//                     if (nums[i] + nums[j] + nums[k] === 0) {
+//                         res.add(JSON.stringify([nums[i], nums[j], nums[k]]))
+//                     }
+//                 }
+//             }
+//         }
+//         return Array.from(res).map(item => JSON.parse(item))
+//     }
+// }
+
+// two pointer
 class Solution {
     /**
      * @param {number[]} nums
      * @return {number[][]}
      */
     threeSum(nums) {
-        const res = new Set()
         nums.sort((a,b) => a - b)
+        
+        const res = []
+
         for (let i = 0; i < nums.length; i++) {
-            for (let j = i + 1; j < nums.length; j++) {
-                for (let k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] === 0) {
-                        res.add(JSON.stringify([nums[i], nums[j], nums[k]]))
-                    }
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] === nums[i - 1]) continue
+
+            let l = i + 1
+            let r = nums.length - 1
+            while (l < r) {
+                const sum = nums[i] + nums[l] + nums[r]
+                if (sum > 0) {
+                    r--
+                } else if (sum < 0) {
+                    l++
+                } else {
+                    res.push([nums[i], nums[l], nums[r]])
+                    l++
+                    r--
+                    while (l < r && nums[l] === nums[l - 1])
+                        l++
                 }
             }
         }
-        return Array.from(res).map(item => JSON.parse(item))
+        return res
     }
 }
