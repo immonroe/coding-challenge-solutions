4b5510703bbf10076bcc7e8d979841cb9f1b3405
immonroe <immanuelmonroe@gmail.com>
2025-07-11 15:06:14 -0400

    Changed comment from ascii to unicode for clarity

---
+++ arraysAndHashing/groupAnagrams.py
@@ -0,0 +1,38 @@
+# Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+# Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+# Example 2:
+
+# Input: strs = ["x"]
+
+# Output: [["x"]]
+# Example 3:
+
+# Input: strs = [""]
+
+# Output: [[""]]
+# Constraints:
+
+# 1 <= strs.length <= 1000.
+# 0 <= strs[i].length <= 100
+# strs[i] is made up of lowercase English letters.
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) # mapping charCount to list of Anagrams
+
+        for s in strs:
+            count = [0] * 26 # a ... z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1 # using ascii value to subtract
+            
+            res[tuple(count)].append(s) # group  anagrams by count, tuples can for store keys (need for this part)
+
+        return list(res.values()) # don't need keys so turning to normal list
