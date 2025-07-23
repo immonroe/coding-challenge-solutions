9ce5a3b738a4da67f4b64ac5b71a5c9b5c31c4f2
immonroe <immanuelmonroe@gmail.com>
2025-07-23 10:38:26 -0400

    Added new solution for sum of positive using python

---
+++ 8kyu/sumOfPositive.py
@@ -0,0 +1,24 @@
+# Task
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example
+# [1, -4, 7, 12] => 
+# 1
+# +
+# 7
+# +
+# 12
+# =
+# 20
+# 1+7+12=20
+# Note
+# If there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    # p = []
+    # for i in arr:
+    #     if arr[i] > 0:
+    #         p.append(arr[i])
+    # return sum(p)
+
+    return sum(x for x in arr if x > 0)
