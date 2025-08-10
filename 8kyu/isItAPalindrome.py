70955b98a57b2adb2f78a6e0f336c0839d81d7e3
immonroe <immanuelmonroe@gmail.com>
2025-08-10 10:00:59 -0400

    Refactored palindrome coding challenge in Python

---
+++ 8kyu/isItAPalindrome.py
@@ -0,0 +1,8 @@
+# Is it a palindrome?
+
+# Write a function that checks if a given string (case insensitive) is a palindrome.
+
+# A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+def isPalindrome(s):
+    return s.lower() == ''.join(reversed(s.lower()))
