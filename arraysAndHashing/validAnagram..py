e42d5fe29f9410338480e345ca2f3e0d31836edd
immonroe <immanuelmonroe@gmail.com>
2025-09-16 08:53:21 -0400

    Updated solution to valid anagram using hash maps

--- arraysAndHashing/validAnagram..py
+++ arraysAndHashing/validAnagram..py
@@ -18,4 +18,12 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return False if len(s) != len(t) else sorted(s.lower()) == sorted(t.lower())
+
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
