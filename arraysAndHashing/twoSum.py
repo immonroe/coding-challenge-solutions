a088e175486a67559ad1807e1204e63658d848ea
immonroe <immanuelmonroe@gmail.com>
2025-09-17 08:35:39 -0400

    Refactored answer to twoSum neetcode question

--- arraysAndHashing/twoSum.py
+++ arraysAndHashing/twoSum.py
@@ -30,7 +30,7 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {} # val -> index
+        prevMap = {}
 
         for i,n in enumerate(nums):
             diff = target - n
