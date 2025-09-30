ab9bf9d8d650970185a465be6e2519dd7c97110f
immonroe <immanuelmonroe@gmail.com>
2025-09-30 09:29:57 -0400

    Completed longest sub str w/o repeating chars using sliding windows and a hashmap

---
+++ slidingWindow/longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,34 @@
+# Longest Substring Without Repeating Characters
+# Solved 
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+# A substring is a contiguous sequence of characters within a string.
+
+# Example 1:
+
+# Input: s = "zxyzxyz"
+
+# Output: 3
+# Explanation: The string "xyz" is the longest without duplicate characters.
+
+# Example 2:
+
+# Input: s = "xxxx"
+
+# Output: 1
+# Constraints:
+
+# 0 <= s.length <= 1000
+# s may consist of printable ASCII characters.
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        l, res = 0,0
+
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(mp[s[r]] + 1, l)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
