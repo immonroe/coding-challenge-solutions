646c6c475216e31e6330323b29715415ce9c93fd
immonroe <immanuelmonroe@gmail.com>
2025-10-06 09:15:20 -0400

    Added solution for valid parenthesis using stack

--- stack/validParentheses.py
+++ stack/validParentheses.py
@@ -31,13 +31,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        # BRUTE FORCE
-        # while '()' in s or '{}' in s or '[]' in s:
-        #     s = s.replace('()', '')
-        #     s = s.replace('{}', '')
-        #     s = s.replace('[]', '')
-        # return s == ''
-
         stack = []
         closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
 
