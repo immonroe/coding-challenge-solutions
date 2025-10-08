eba6bdba698d5c1ab804432b9bc1c152edb00dc6
immonroe <immanuelmonroe@gmail.com>
2025-10-08 11:34:44 -0400

    Added solution for evaluate reverse polish notation

--- stack/evaluateReversePolishNotation.py
+++ stack/evaluateReversePolishNotation.py
@@ -1,3 +1,5 @@
+# Evaluate Reverse Polish Notation
+
 # You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
 
 # Return the integer that represents the evaluation of the expression.
